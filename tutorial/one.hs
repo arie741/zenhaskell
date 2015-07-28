1a8c9868676b5b2ce0213a9ddd4eb1898e515c8f
squest <sabdaps@gmail.com>
2015-07-29 00:43:45 +0700

    some samples

--- tutorial/one.hs
+++ tutorial/one.hs
@@ -7,8 +7,12 @@ cube x = x*x*x
 
 thisIsAVariable = 123
 
-fak1 i = if i == 0 then 1 else i * (fak1 (pred i))
+fak1 i = if i == 0 then 1 else i * (fak1 $ pred i)
 
 fak2 i
   | i == 0 = 1
-  | otherwise = i * (fak1 (pred i))
+  | otherwise = i * (fak1 $ pred i)
+
+fak3 0 = 1
+fak3 2 = 2
+fak3 i = i * (fak3 $ pred i)
